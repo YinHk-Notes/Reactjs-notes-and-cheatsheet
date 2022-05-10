9e4b534ddaafdb5a5967e6cac3b9e1a0fac6b2b9
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-09 23:26:02 -0400

    Update Hook.jsx

--- cheatsheet/Hook/Hook.jsx
+++ cheatsheet/Hook/Hook.jsx
@@ -1,3 +1,6 @@
+//Hooks are a new addition in React 16.8. They let you use state and other React features without writing a class.
+
+
 // useState
 const [ statObj, setStateObj ] = useState({v1: 0, v2: 1})
 // setState
