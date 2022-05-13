be5f042627e37e8b9d43c75a9d8fbd895ef982a7
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-13 19:48:14 -0400

    Update React-component.jsx

--- cheatsheet/React API/React-component.jsx
+++ cheatsheet/React API/React-component.jsx
@@ -1,1 +1,14 @@
+/* React.Component */
+// base class for React components
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+/* React.PureComponent */
+// React.PureComponent is similar to React.Component
+// The difference between them is that React.Component doesn’t implement shouldComponentUpdate(), but React.PureComponent implements it with a shallow prop and state comparison.
+
+
 
