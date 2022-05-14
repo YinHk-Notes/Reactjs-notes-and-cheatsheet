19710b825cd0a1c14d7f35bc1b9a2ad6482b47e7
Ken <67538811+YinHk@users.noreply.github.com>
2022-05-13 22:07:58 -0400

    Update classComponent.js

--- cheatsheet/components/classComponent.js
+++ cheatsheet/components/classComponent.js
@@ -1,1 +1,19 @@
+class App extends React.Component{ 
+   constructor(props){
+    super(props);
+    this.state = { };
+    //...
+   }
+   
+   render() {
+     return (
+      <></>
+     
+     )
+   
+   }
+  
 
+}
+
+export default App;
