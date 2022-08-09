f7154e260715649ca8c81711c2340dba60c1c3e6
Ken <67538811+YinHk@users.noreply.github.com>
2022-08-09 19:46:48 -0400

    Update React.jsx

--- cheatsheet/React API/React.jsx
+++ cheatsheet/React API/React.jsx
@@ -1,6 +1,11 @@
 /* React.Component */
 // base class for React components
 class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+    //...
+  }
   render() {
     return <h1>Hello, {this.props.name}</h1>;
   }
