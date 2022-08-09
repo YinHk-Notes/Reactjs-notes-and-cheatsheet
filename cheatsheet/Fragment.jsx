2c8f789ec2bb99955f5a4941e6c2eec6182fc51f
Ken <67538811+YinHk@users.noreply.github.com>
2022-08-08 22:18:18 -0400

    Update Fragment.jsx

--- cheatsheet/Fragment.jsx
+++ cheatsheet/Fragment.jsx
@@ -1,5 +1,11 @@
+// React components can only render one element, and if you have multiple elements, 
+// the common practice is to wrap them in a single root element, usually a <div> or <></> wrapper.
+// This workaround works for most cases, but sometimes adding an extra DOM element is not feasible.
+
+// React Fragments allow you to wrap or group multiple elements without adding an extra node to the DOM.
 // react fragment is used to wrap multiple elements into one node ,
 // beacause return only accept one node
+
 
 return (
     <React.Fragment>
